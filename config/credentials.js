1f2820da70fb5fdc0e5fddd8ab9b415576ecc5f2
orland <93163969+orlandu1@users.noreply.github.com>
2024-12-02 23:49:39 -0300

    Update credentials.js

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  CLIENT_ID: '60896297333-2ipbmrrfbe1mhq7gd3buukq7fnm0c104.apps.googleusercontent.com',
-  CLIENT_SECRET: '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',
-  REDIRECT_URI: 'http://localhost:3000', // Certifique-se de que está correto
+  CLIENT_ID: '',
+  CLIENT_SECRET: '',
+  REDIRECT_URI: '', // Certifique-se de que está correto
 };
